adda905938a2dc053bab67cf41dec9cfe48227d1
KalechMed <medkalech@gmail.com>
2023-09-03 15:25:58 +0100

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Mohamed  on 3/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
