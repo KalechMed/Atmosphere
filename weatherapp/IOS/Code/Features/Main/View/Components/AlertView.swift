a92f5f388b5fded68434d9b40687c1a00c435106
KalechMed <medkalech@gmail.com>
2023-09-11 20:32:20 +0100

    alert UI added

---
+++ weatherapp/IOS/Code/Features/Main/View/Components/AlertView.swift
@@ -0,0 +1,18 @@
+//
+//  AlertView.swift
+//  weatherapp
+//
+//  Created by Mohamed  on 11/9/2023.
+//
+
+import SwiftUI
+
+struct AlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertView()
+}
